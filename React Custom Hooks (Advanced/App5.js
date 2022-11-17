9c3171c69917cbf376fb1c038e127ba91c3a07f8
ChadCorpin <116444454+ChadCorpin@users.noreply.github.com>
2022-11-17 19:25:54 +0800

    Create App5.js

---
+++ React Custom Hooks (Advanced/App5.js
@@ -0,0 +1,17 @@
+const useSemiPersistentState = (key, initialState) => {
+const [value, setValue] = React.useState(
+localStorage.getItem(key) || initialState
+);
+React.useEffect(() => {
+localStorage.setItem(key, value);
+}, [value, key]);
+return [value, setValue];
+};
+const App = () => {
+...
+const [searchTerm, setSearchTerm] = useSemiPersistentState(
+'search',
+'React'
+);
+...
+};
